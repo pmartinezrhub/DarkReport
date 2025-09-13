5b3654ba4c941958f1ccafb3af3d004cb5f3859d
pablo <pmartinezr@proton.me>
2025-09-13 17:27:45 +0200

    settings for demo

--- DarkReport/settings.py
+++ DarkReport/settings.py
@@ -46,7 +46,7 @@ MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.locale.LocaleMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware', # deactivate for demo
+    #'django.middleware.csrf.CsrfViewMiddleware', # deactivate for demo
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
